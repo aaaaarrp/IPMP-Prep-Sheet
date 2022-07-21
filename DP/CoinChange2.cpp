d54bfebeae494b59eda304a7d35fa9bfad32b98f
Arpon Kapuria <arpon@example.com>
2022-07-21 16:07:32 +0600

    Medium

--- DP/CoinChange2.cpp
+++ DP/CoinChange2.cpp
@@ -14,8 +14,8 @@ int minCoinNeeded(int n, int amount, vector<int> &coins){
 
     for(int i=0; i<amount+1; i++)
         table[0][i] = INT_MAX-1;    // INT_MAX-1 to avoid overflow
-    for(int i=0; i<n+1; i++)
-        table[i][0] = 0;
+    for(int i=0; i<n+1; i++)        // As we're adding 1 coin in inclusive part
+        table[i][0] = 0;            // So INT_MAX-1 + 1 = INT_MAX
 
     for(int i=1; i<n+1; i++){
         for(int j=1; j<amount+1; j++){
